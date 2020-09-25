566543f579e8c7bb1511092e5776465bb9c66109
Miguel <amahrok@live.com>
2020-09-24 21:55:34 -0500

    Turning in assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,4 +43,20 @@ constructor() {
         }.bind(this));
       }.bind(this));
 }
+
+
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
+
 }
